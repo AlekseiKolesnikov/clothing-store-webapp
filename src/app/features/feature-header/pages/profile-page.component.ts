000d918a80a50bd6ad24e33b180772a0094ec7d7
Aleksei Kolesnikov <124324859+AlekseiKolesnikov@users.noreply.github.com>
2023-10-04 09:12:27 -0500

    Была сделанна страница профиля profile-page.component.ts

--- src/app/features/feature-header/pages/profile-page.component.ts
+++ src/app/features/feature-header/pages/profile-page.component.ts
@@ -1,11 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import {environment} from "../../../../environments/environment";
+import {BaseComponent} from "../../../shared/models/base-component.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
-  styleUrls: ['./profile-page.component.scss']
+  styleUrls: ['./profile-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class ProfilePageComponent {
-  constructor() {
+export class ProfilePageComponent extends BaseComponent {
+  readonly profileEnvironment = environment
+
+  constructor(
+    private router: Router
+  ) {
+    super();
+  }
+
+  override click(event: Event) {
+    super.click(event);
   }
 }
