15c59449ee1bae57b23bc3e6dd47cd45876f0615
Aleksei Kolesnikov <124324859+AlekseiKolesnikov@users.noreply.github.com>
2023-10-15 16:12:00 -0500

    Исправил баг с не появлением ФИО и телефон строк если ничего не было введено

--- src/app/features/feature-profile/components/profile-delivery-inf/profile-delivery-inf.component.ts
+++ src/app/features/feature-profile/components/profile-delivery-inf/profile-delivery-inf.component.ts
@@ -3,33 +3,33 @@ import {environment} from "../../../../../environments/environment.development";
 import {InputButtonStateService} from "../../service/input-button-state.service";
 
 @Component({
-      selector: 'app-profile-delivery-inf',
-      templateUrl: './profile-delivery-inf.component.html',
-      styleUrls: ['./profile-delivery-inf.component.scss'],
-      encapsulation: ViewEncapsulation.None
+  selector: 'app-profile-delivery-inf',
+  templateUrl: './profile-delivery-inf.component.html',
+  styleUrls: ['./profile-delivery-inf.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ProfileDeliveryInfComponent {
-      numberFieldIsShown: boolean
-      nameFieldIsShown: boolean
-      protected readonly environment = environment;
+  numberFieldIsShown: boolean
+  nameFieldIsShown: boolean
+  protected readonly environment = environment;
 
-      constructor(
-            private inputButtonStateService: InputButtonStateService
-      ) {
-            this.inputButtonStateService.getNameButtonState().subscribe(value => {
-                  this.nameFieldIsShown = value
-            })
-            this.inputButtonStateService.getPhoneButtonState().subscribe(value => {
-                  this.numberFieldIsShown = value
-            })
-      }
+  constructor(
+    private inputButtonStateService: InputButtonStateService
+  ) {
+    this.inputButtonStateService.getNameButtonState().subscribe(value => {
+      this.nameFieldIsShown = value
+    })
+    this.inputButtonStateService.getPhoneButtonState().subscribe(value => {
+      this.numberFieldIsShown = value
+    })
+  }
 
-      switchInputState(inputFieldType: string): void {
-            if (inputFieldType === 'number') {
-                  this.inputButtonStateService.setPhoneButtonState(true)
-            }
-            if (inputFieldType === 'name') {
-                  this.inputButtonStateService.setNameButtonState(true)
-            }
-      }
+  switchInputState(inputFieldType: string): void {
+    if (inputFieldType === 'number') {
+      this.inputButtonStateService.setPhoneButtonState(true)
+    }
+    if (inputFieldType === 'name') {
+      this.inputButtonStateService.setNameButtonState(true)
+    }
+  }
 }
--- src/app/features/feature-profile/components/profile-input-field/profile-input-field.component.ts
+++ src/app/features/feature-profile/components/profile-input-field/profile-input-field.component.ts
@@ -3,36 +3,42 @@ import {environment} from "../../../../../environments/environment.development";
 import {InputButtonStateService} from "../../service/input-button-state.service";
 
 @Component({
-      selector: 'app-profile-input-field',
-      templateUrl: './profile-input-field.component.html',
-      styleUrls: ['./profile-input-field.component.scss'],
-      encapsulation: ViewEncapsulation.None
+  selector: 'app-profile-input-field',
+  templateUrl: './profile-input-field.component.html',
+  styleUrls: ['./profile-input-field.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ProfileInputFieldComponent {
-      @Input() inputTextLength: number
-      @Input() inputType: string
-      @Input() buttonPlaceholder: string
-      @Input() inputName: string
+  @Input() inputTextLength: number
+  @Input() inputType: string
+  @Input() buttonPlaceholder: string
+  @Input() inputName: string
 
-      inputFiledIsShown: boolean = false
-      protected readonly environment = environment;
+  inputFiledIsShown: boolean = false
+  protected readonly environment = environment;
 
-      constructor(
-            private inputButtonStateService: InputButtonStateService
-      ) {
-      }
+  constructor(
+    private inputButtonStateService: InputButtonStateService
+  ) {
+  }
 
-      switchInputState(inputFieldType: string): void {
-            if (inputFieldType === 'number') {
-                  this.inputButtonStateService.setPhoneButtonState(false)
-            }
-            if (inputFieldType === 'name') {
-                  this.inputButtonStateService.setNameButtonState(false)
-            }
-      }
+  switchInputState(inputFieldType: string): void {
+    if (inputFieldType === 'number') {
+      this.inputButtonStateService.setPhoneButtonState(false)
+    }
+    if (inputFieldType === 'name') {
+      this.inputButtonStateService.setNameButtonState(false)
+    }
+  }
 
-      switchFormDataState() {
-            this.inputFiledIsShown = !this.inputFiledIsShown
-            console.log(environment.profileData.personalFullName)
-      }
+  switchFormDataState() {
+    this.inputFiledIsShown = !this.inputFiledIsShown
+
+    if (this.environment.profileData.personalFullName.length === 0) {
+      this.inputButtonStateService.setNameButtonState(false)
+    }
+    if (this.environment.profileData.personalPhoneNumber.length === 0) {
+      this.inputButtonStateService.setPhoneButtonState(false)
+    }
+  }
 }
