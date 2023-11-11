d70f2e686cac7daf13e7ab31b27c0bcd147c2f23
Aleksei Kolesnikov <124324859+AlekseiKolesnikov@users.noreply.github.com>
2023-11-11 17:14:27 -0600

    Перенёс многое в переменные и доделал friends-card-page.component.html сделал там таблицу

---
+++ src/app/features/feature-friends-card/data/friends-page-table.data.ts
@@ -0,0 +1,18 @@
+export const friendsPageTableData = [
+  {
+    text: "Переходов по ссылке",
+    amount: 0
+  },
+  {
+    text: "Сделали заказ",
+    amount: 0
+  },
+  {
+    text: "Отправили в РФ",
+    amount: 0
+  },
+  {
+    text: "Всего баллов заработанно",
+    amount: 0
+  }
+]
