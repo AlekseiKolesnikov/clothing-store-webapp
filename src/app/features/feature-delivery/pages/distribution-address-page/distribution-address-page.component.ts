1012f00140c060e7ed0d4a18ce48493a6bf93bef
Aleksei Kolesnikov <124324859+AlekseiKolesnikov@users.noreply.github.com>
2023-12-16 20:00:19 -0600

    Работаю над distribution-address-page.component.ts

---
+++ src/app/features/feature-delivery/pages/distribution-address-page/distribution-address-page.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-distribution-address-page',
+  templateUrl: './distribution-address-page.component.html',
+  styleUrls: ['./distribution-address-page.component.scss']
+})
+export class DistributionAddressPageComponent {
+  constructor() {
+  }
+}
